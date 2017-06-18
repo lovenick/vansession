39841e07161d3d0facd76f3e5231015f9d7b717a
Nick Loveridge <nick@fusemedia.ca>
2017-06-18 14:26:10 -0700

    Added contentful

--- build.js
+++ build.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const Metalsmith = require('metalsmith');
 const markdown   = require('metalsmith-markdown');
 const templates  = require('metalsmith-templates');
+const contentful = require('contentful-metalsmith')({ 'access_token' : process.env.CONTENTFUL_TOKEN })
 
 Metalsmith(__dirname)
   .use(markdown())
   .use(templates('handlebars'))
+  .use(contentful)
   .destination('./public')
   .build((err) => {
     if(err) console.log(err);
